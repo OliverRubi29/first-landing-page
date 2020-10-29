6c3f58315d694115bcff4a860db5d8b79cea69a8
OliverRubi29 <oliver.rubi13@gmail.com>
2020-10-29 17:16:38 +0800

    Updates

---
+++ precache-manifest.a7c67112812536dd0a520d372543092f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "723a2b50bbe4b6890f777286f7df7127",
+    "url": "/first-landing-page/index.html"
+  },
+  {
+    "revision": "4e7a96d7b95f6902a3b5",
+    "url": "/first-landing-page/static/css/main.48a84243.chunk.css"
+  },
+  {
+    "revision": "9ae4b478e0130b37a780",
+    "url": "/first-landing-page/static/js/2.c94d8e64.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/first-landing-page/static/js/2.c94d8e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7a96d7b95f6902a3b5",
+    "url": "/first-landing-page/static/js/main.e9294651.chunk.js"
+  },
+  {
+    "revision": "dc3c0df5197b1fe160bf",
+    "url": "/first-landing-page/static/js/runtime-main.2d034f44.js"
+  },
+  {
+    "revision": "2f63b1e920f288630b2ff8a1bb56ff4a",
+    "url": "/first-landing-page/static/media/cover.2f63b1e9.jpeg"
+  },
+  {
+    "revision": "cc16b987c5316d01f7b5d0f0e2a2210c",
+    "url": "/first-landing-page/static/media/undraw_Terms_re_6ak4.cc16b987.svg"
+  },
+  {
+    "revision": "06325cd9f6d489659cf896b5bb3ab22a",
+    "url": "/first-landing-page/static/media/undraw_mobile_development_8gyo.06325cd9.svg"
+  },
+  {
+    "revision": "c9c1ecdcea440c847b5e599e01ca6dbb",
+    "url": "/first-landing-page/static/media/undraw_mobile_user_7oqo.c9c1ecdc.svg"
+  },
+  {
+    "revision": "1d7c73de6b88b1148f7a440710590231",
+    "url": "/first-landing-page/static/media/video.1d7c73de.mp4"
+  }
+]);
